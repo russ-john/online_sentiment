3717f8a32bef934d986ddb5528c0d7d5a127f704
Cris Giovanoni <cris.giovanoni@finra.org>
2025-01-20 16:12:40 -0600

    Update EmergingRiskNews.py

--- EmergingRiskNews.py
+++ EmergingRiskNews.py
@@ -59,7 +59,7 @@ for u_agent in user_agent_list:
 # read in encoded alerts and create empty lists for storing values
 read_file = pd.read_csv('EmergingRisksListEncoded.csv', encoding='utf-8')
 read_file['EMERGING_RISK_ID'] = pd.to_numeric(read_file['EMERGING_RISK_ID'], downcast='integer', errors='coerce')
-read_file['SEARCH_TERMS'] = read_file.ENCODED_TERMS.to_bytes(int_txt.bit_length() + 7 // 8, byteorder: 'little').decode()
+read_file['SEARCH_TERMS'] = read_file.ENCODED_TERMS.to_bytes(read_file.ENCODED_TERMS.bit_length() + 7 // 8, byteorder: 'little').decode()
 read_file.columns = read_file.columns.str.strip()
 
 search_terms = []
